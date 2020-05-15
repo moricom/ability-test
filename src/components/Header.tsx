4214b10222df3258b5ce78e76a332b1348269df2
mori <mori.ryusei@artifacture.co.jp>
2020-05-15 22:26:35 +0900

    問題文実装

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return <div style={{ height: '2rem', background: '#7E57C2' }}></div>;
+};
+
+export default Header;
--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -2,7 +2,7 @@ import React, { Children } from 'react';
 import ramen from '../img/ramen.png';
 import styled from 'styled-components';
 import fetchJsonp from 'fetch-jsonp';
-import { type } from 'os';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
 interface zipResponseResult {
   address1: string; //"東京都"
@@ -39,34 +39,50 @@ const Container = styled.div`
   padding: 0 1rem 0 1rem;
 `;
 
-interface itemProps {
-  select: boolean;
-}
-
 const Item = styled.div<{ select?: boolean }>`
+  position: relative;
   padding: 1rem 0 1rem 0;
   border: solid #bdbdbd;
   border-width: 0 0 1px 0;
   background: ${(props) => (props.select ? '#E3F2FD' : 'none')};
 `;
 
+const Icon = styled(InfoOutlinedIcon)<{ select?: boolean }>`
+  color: ${(props) => (props.select ? 'gray' : 'black')};
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 1rem;
+  height: 1rem;
+`;
+
+interface IH {
+  p: boolean;
+  color: boolean;
+  img: boolean;
+  link: boolean;
+  inline: boolean;
+}
+
 const Preview: React.FC = () => {
   const [color1, setColor1] = React.useState(true);
   const [text1, setText1] = React.useState(true);
   const text1Data = {
-    true: 'ボタンをクリックしてテキストを変更',
-    false: '変更しました',
+    true: '変更前のテキスト',
+    false: 'クリックされた',
   };
   const [input1, setInput1] = React.useState('ユーザーの入力');
   const [bool1, setBool1] = React.useState(true);
   const [zip, setZip] = React.useState('1670043');
   const [address, setAddress] = React.useState('東京都杉並区上荻');
-  const [s, setSelects] = React.useState({
+  const [h, setH] = React.useState<IH>({
     p: false,
     color: false,
     img: false,
     link: false,
     inline: false,
+  });
+  const [s, setSelects] = React.useState({
     jsColor: false,
     jQColor: false,
     bColor: false,
@@ -77,176 +93,386 @@ const Preview: React.FC = () => {
     bool: false,
     loop: false,
     obj: false,
+  });
+
+  const [a, setA] = React.useState({
     ajax: false,
   });
+
+  const [titleClick, setTitleClick] = React.useState({
+    html: false,
+    js: false,
+    ajax: false,
+  });
+
+  const onTitleHTMLClick = () => {
+    // if (!titleClick.html) {
+    //   setH({ ...h });
+    //   let result: IH = { ...h };
+    //   for (let key in result) {
+    //     result[key] = true;
+    //   }
+    //   setTitleClick({ ...titleClick, html: true });
+    // } else {
+    //   setSelects({ ...s, p: false, color: false, img: false, link: false, inline: false });
+    //   setTitleClick({ ...titleClick, html: false });
+    // }
+  };
+
+  const onTitleJSClick = () => {
+    if (!titleClick.html) {
+      setSelects({
+        ...s,
+        jsColor: true,
+        jQColor: true,
+        bColor: true,
+        bText: true,
+        fAlert: true,
+        mAlert: true,
+        iText: true,
+        bool: true,
+        loop: true,
+        obj: true,
+      });
+      setTitleClick({ ...titleClick, js: true });
+    } else {
+      setSelects({
+        ...s,
+        jsColor: false,
+        jQColor: false,
+        bColor: false,
+        bText: false,
+        fAlert: false,
+        mAlert: false,
+        iText: false,
+        bool: false,
+        loop: false,
+        obj: false,
+      });
+      setTitleClick({ ...titleClick, js: false });
+    }
+  };
 
   return (
     <Page>
       <Container>
         <Item>
+          <Icon onClick={onTitleHTMLClick} />
           <h3>HTML</h3>
         </Item>
-        <Item
-          select={s.p}
-          onClick={() => {
-            setSelects({ ...s, p: !s.p });
-          }}
-        ></Item>
-        <Item
-          select={s.color}
-          onClick={() => {
-            setSelects({ ...s, color: !s.color });
-          }}
-        >
-          <p>文字色の変更</p>
+
+        <Item select={h.p}>
+          <Icon
+            select={h.p}
+            onClick={() => {
+              setH({ ...h, p: !h.p });
+            }}
+          />
+          {h.p ? <p>文字を表示する</p> : <p>テキスト</p>}
         </Item>
-        <Item
-          select={s.img}
-          onClick={() => {
-            setSelects({ ...s, img: !s.img });
-          }}
-        >
-          <img style={{ width: '10rem' }} src={ramen} />
+
+        <Item select={h.color}>
+          <Icon
+            select={h.color}
+            onClick={() => {
+              setH({ ...h, color: !h.color });
+            }}
+          />
+          {h.color ? <p>文字色を指定する</p> : <p style={{ color: '#FFC107' }}>テキスト</p>}
         </Item>
-        <Item
-          select={s.link}
-          onClick={() => {
-            setSelects({ ...s, link: !s.link });
-          }}
-        >
-          <a href="https://www.google.com/">Google.com</a>
+
+        <Item select={h.img}>
+          <Icon
+            select={h.img}
+            onClick={() => {
+              setH({ ...h, img: !h.img });
+            }}
+          />
+          {h.img ? <p>画像を表示する</p> : <img style={{ width: '10rem' }} src={ramen} />}
         </Item>
-        <Item
-          select={s.inline}
-          onClick={() => {
-            setSelects({ ...s, inline: !s.inline });
-          }}
-        >
-          <span style={{ background: '#ffcdd2' }}>インライン</span>
-          <span style={{ background: '#C5CAE9' }}>要素</span>
+
+        <Item select={h.link}>
+          <Icon
+            select={h.link}
+            onClick={() => {
+              setH({ ...h, link: !h.link });
+            }}
+          />
+          {h.link ? (
+            <>
+              <p>下記のリンクを使用してリンクの実装</p>
+              <p>https://www.google.com/</p>
+            </>
+          ) : (
+            <a href="https://www.google.com/" target="blank">
+              Google.com
+            </a>
+          )}
+        </Item>
+
+        <Item select={h.inline}>
+          <Icon
+            select={h.inline}
+            onClick={() => {
+              setH({ ...h, inline: !h.inline });
+            }}
+          />
+          {h.inline ? (
+            <p>インライン要素にそれぞれ背景色を指定する</p>
+          ) : (
+            <>
+              <span style={{ background: '#ffcdd2' }}>インライン</span>
+              <span style={{ background: '#C5CAE9' }}>要素</span>
+            </>
+          )}
         </Item>
       </Container>
+
       <Container>
         <Item>
           <h3>JavaScript</h3>
         </Item>
-        <Item
-          select={s.jsColor}
-          onClick={() => {
-            setSelects({ ...s, jsColor: !s.jsColor });
-          }}
-        >
-          <p style={{ color: '#AB47BC' }}>JSから色を変更する</p>
-        </Item>
-        <Item
-          select={s.p}
-          onClick={() => {
-            setSelects({ ...s, p: !s.p });
-          }}
-        >
-          <p style={{ color: '#5C6BC0' }}>jQueryから色を変える</p>
-        </Item>
-        <Item>
-          <p style={color1 ? { color: '#4CAF50' } : { color: 'white' }}>
-            ボタンをクリックして色を変える
-          </p>
-          <button
-            style={{ marginLeft: '1rem' }}
-            onClick={() => {
-              setColor1(!color1);
-            }}
-          >
-            Button
-          </button>
-        </Item>
-        <Item>
-          <span>{text1 ? text1Data.true : text1Data.false}</span>
-          <button
-            style={{ marginLeft: '1rem' }}
-            onClick={() => {
-              setText1(!text1);
-            }}
-          >
-            Button
-          </button>
-        </Item>
-        <Item>
-          <p>自作の関数を作りボタンをクリックしてアラート</p>
-          <button
-            onClick={() => {
-              alert('ボタンがクリックされました');
-            }}
-          >
-            Button
-          </button>
-        </Item>
 
-        <Item>
-          <p>メソッドを作成しボタンをクリックしてアラート</p>
-          <button
+        <Item select={s.jsColor}>
+          <Icon
+            select={s.jsColor}
             onClick={() => {
-              alert('ボタンがクリックされました');
-            }}
-          >
-            Button
-          </button>
-        </Item>
-
-        <Item>
-          <input
-            value={input1}
-            onChange={(e) => {
-              setInput1(e.target.value);
+              setSelects({ ...s, jsColor: !s.jsColor });
             }}
           />
-          <p>{input1}</p>
+          {s.jsColor ? <p>JSから色を変更する</p> : <p style={{ color: '#FFC107' }}>テキスト</p>}
         </Item>
 
-        <Item>
-          <p>ボタンを押したら変数 bool を True, False と切り替える</p>
-          <button onClick={() => setBool1(!bool1)}>Button</button>
-          <p>変数 boolの値によってテキストが変わる</p>
-          <p>{String(bool1)}</p>
+        <Item select={s.jQColor}>
+          <Icon
+            select={s.jQColor}
+            onClick={() => {
+              setSelects({ ...s, jQColor: !s.jQColor });
+            }}
+          />
+          {s.jQColor ? (
+            <p>jQueryを使用して色を変える</p>
+          ) : (
+            <p style={{ color: '#FFC107' }}>テキスト</p>
+          )}
         </Item>
-        <Item>
-          <p>ループで1~10の数字を表示する。10で折り返す</p>
-          <p>1 2 3 4 5 6 7 8 9 10 9 8 7 6 5 4 3 2 1</p>
+
+        <Item select={s.bColor}>
+          <Icon
+            select={s.bColor}
+            onClick={() => {
+              setSelects({ ...s, bColor: !s.bColor });
+            }}
+          />
+          {s.bColor ? (
+            <p>ボタンをクリックして色を変える</p>
+          ) : (
+            <>
+              <p style={color1 ? { color: '#4CAF50' } : {}}>テキスト</p>
+              <button
+                onClick={() => {
+                  setColor1(!color1);
+                }}
+              >
+                Button
+              </button>
+            </>
+          )}
         </Item>
-        <Item>
-          <p>"Key"に"value"をもつオブジェクトを作る</p>
+
+        <Item select={s.bText}>
+          <Icon
+            select={s.bText}
+            onClick={() => {
+              setSelects({ ...s, bText: !s.bText });
+            }}
+          />
+          {s.bText ? (
+            <p>ボタンをクリックしてテキストを変える</p>
+          ) : (
+            <>
+              <p>{text1 ? text1Data.true : text1Data.false}</p>
+              <button
+                onClick={() => {
+                  setText1(!text1);
+                }}
+              >
+                Button
+              </button>
+            </>
+          )}
+        </Item>
+
+        <Item select={s.fAlert}>
+          <Icon
+            select={s.fAlert}
+            onClick={() => {
+              setSelects({ ...s, fAlert: !s.fAlert });
+            }}
+          />
+          {s.fAlert ? (
+            <p>自作の関数を作りボタンをクリックしてアラート</p>
+          ) : (
+            <>
+              <p>アラート</p>
+              <button
+                onClick={() => {
+                  alert('ボタンがクリックされました');
+                }}
+              >
+                Button
+              </button>
+            </>
+          )}
+        </Item>
+
+        <Item select={s.mAlert}>
+          <Icon
+            select={s.mAlert}
+            onClick={() => {
+              setSelects({ ...s, mAlert: !s.mAlert });
+            }}
+          />
+          {s.mAlert ? (
+            <p>メソッドを作成しボタンをクリックしてアラート</p>
+          ) : (
+            <>
+              <p>アラート</p>
+              <button
+                onClick={() => {
+                  alert('ボタンがクリックされました');
+                }}
+              >
+                Button
+              </button>
+            </>
+          )}
+        </Item>
+
+        <Item select={s.iText}>
+          <Icon
+            select={s.iText}
+            onClick={() => {
+              setSelects({ ...s, iText: !s.iText });
+            }}
+          />
+          {s.iText ? (
+            <p>ユーザーの入力を受け取って表示する</p>
+          ) : (
+            <>
+              <input
+                value={input1}
+                onChange={(e) => {
+                  setInput1(e.target.value);
+                }}
+              />
+              <p>{input1}</p>
+            </>
+          )}
+        </Item>
+
+        <Item select={s.bool}>
+          <Icon
+            select={s.bool}
+            onClick={() => {
+              setSelects({ ...s, bool: !s.bool });
+            }}
+          />
+          {s.bool ? (
+            <>
+              <p>変数 bool を宣言。ボタンをクリックで True, False と切り替える</p>
+              <p>変数 bool の値を表示する</p>
+            </>
+          ) : (
+            <>
+              <p>{String(bool1)}</p>
+              <button onClick={() => setBool1(!bool1)}>Button</button>
+            </>
+          )}
+        </Item>
+
+        <Item select={s.loop}>
+          <Icon
+            select={s.loop}
+            onClick={() => {
+              setSelects({ ...s, loop: !s.loop });
+            }}
+          />
+          {s.loop ? (
+            <p>ループで1~10の数字を表示する。10で折り返す</p>
+          ) : (
+            <>
+              <p>1 2 3 4 5 6 7 8 9 10 9 8 7 6 5 4 3 2 1</p>
+            </>
+          )}
+        </Item>
+
+        <Item select={s.obj}>
+          <Icon
+            select={s.obj}
+            onClick={() => {
+              setSelects({ ...s, obj: !s.obj });
+            }}
+          />
+          {s.obj ? (
+            <>
+              <p>fastに"ラーメン", lastに"55" をもつオブジェクトを作る</p>
+              <p>fastとlast を表示する</p>
+            </>
+          ) : (
+            <p>ラーメン55</p>
+          )}
         </Item>
       </Container>
+
       <Container>
         <Item>
           <h3>Ajax</h3>
         </Item>
-        <Item>
-          <p>郵便局のAPIに郵便番号を投げて住所を取得する</p>
-          <input
-            value={zip}
-            onChange={(e) => {
-              setZip(e.target.value);
+        <Item select={a.ajax}>
+          <Icon
+            select={a.ajax}
+            onClick={() => {
+              setA({ ...a, ajax: !a.ajax });
             }}
           />
-          <button
-            onClick={() => {
-              fetchJsonp(`https://zip-cloud.appspot.com/api/search?zipcode=${zip}`)
-                .then((res) => {
-                  return res.json();
-                })
-                .then((data: zipResponse) => {
-                  console.log(data);
-                  const result = data.results[0];
-                  setAddress(result.address1 + result.address2 + result.address3);
-                })
-                .catch((err) => {
-                  console.log(err);
-                });
-            }}
-          >
-            検索
-          </button>
-          <p>{address}</p>
+          {a.ajax ? (
+            <>
+              <p>郵便局のAPIに郵便番号を投げて住所を取得する</p>
+              <a href="http://zipcloud.ibsnet.co.jp/doc/api" target="blank">
+                ドキュメント
+              </a>
+            </>
+          ) : (
+            <>
+              <p>郵便番号</p>
+              <input
+                value={zip}
+                onChange={(e) => {
+                  setZip(e.target.value);
+                }}
+              />
+              <button
+                onClick={() => {
+                  fetchJsonp(`https://zip-cloud.appspot.com/api/search?zipcode=${zip}`)
+                    .then((res) => {
+                      return res.json();
+                    })
+                    .then((data: zipResponse) => {
+                      console.log(data);
+                      const result = data.results[0];
+                      setAddress(result.address1 + result.address2 + result.address3);
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                    });
+                }}
+              >
+                検索
+              </button>
+              <p>{address}</p>
+            </>
+          )}
         </Item>
       </Container>
     </Page>
